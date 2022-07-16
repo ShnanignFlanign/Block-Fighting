ff03d9e755208c73024944340887df7ab566aa5d
ShnanignFlanign <seanpflan@live.com>
2022-07-16 15:01:03 -0700

    jul 16 progress

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ console.log("My app.js file is attached.")
 // --> drain health method 
 
 class Character {
-    constructor(player, character){
+    constructor(player, fighter){
         this.player = player;
-        this.character = character;
+        this.fighter = fighter;
         this.health = 100;
     }
     loseHealth (value) {
@@ -21,6 +21,40 @@ class Character {
     }
     //this is rough draft, might need to add more stuff
 }
+
+const p1Character = new Character('p1','')
+const p2Character = new Character('p2', '')
+
+//html elements in vars
+const h2 = document.querySelector('h2')
+const selectPlayer = document.querySelector('#selectPlayer')
+const p1Button = document.querySelector('#p1')
+const p2Button = document.querySelector('#p2')
+
+//all cosmetic character tags
+const characterContainer = document.querySelector("#characterContainer")
+const boxBoi = document.querySelector('#boxBoi')
+const boxBoiGif = document.querySelector('#boxBoiGif');
+const boxBoiHealth = document.querySelector('#boxBoiHealth');
+const devilDude = document.querySelector('#devilDude')
+const devilDudeGif = document.querySelector('#devilDudeGif');
+const devilDudeHealth = document.querySelector('#devilDudeHealth');
+const flubber = document.querySelector('#flubber')
+const flubberGif = document.querySelector('#flubberGif');
+const flubberHealth = document.querySelector('#flubberHealth');
+const p1Health = document.querySelector("#p1Health");
+const p2Health = document.querySelector("#p2Health");
+
+//character select buttons
+const buttonContainer = document.querySelector('#buttonContainer')
+const boxBoiButton = document.querySelector('#boxBoiButton')
+const devilDudeButton = document.querySelector('#devilDudeButton')
+const flubberButton = document.querySelector('#flubberButton')
+
+// character fight buttons
+const fightButtons = document.querySelector('#fightButtons')
+const p1Attacks = document.querySelector('#p1Attacks')
+const p2Attacks = document.querySelector('#p2Attacks')
 
 highlightSelection = (e) => {
     if(e.target !== selectPlayer){
@@ -56,36 +90,26 @@ highlightSelection = (e) => {
         }
     }
 }
-
-//html elements in vars
-const h2 = document.querySelector('h2')
-const selectPlayer = document.querySelector('#selectPlayer')
-const p1Button = document.querySelector('#p1')
-const p2Button = document.querySelector('#p2')
-
-//all cosmetic character tags
-const characterContainer = document.querySelector("#characterContainer")
-const boxBoi = document.querySelector('#boxBoi')
-const boxBoiGif = document.querySelector('#boxBoiGif');
-const boxBoiHealth = document.querySelector('#boxBoiHealth');
-const devilDude = document.querySelector('#devilDude')
-const devilDudeGif = document.querySelector('#devilDudeGif');
-const devilDudeHealth = document.querySelector('#devilDudeHealth');
-const flubber = document.querySelector('#flubber')
-const flubberGif = document.querySelector('#flubberGif');
-const flubberHealth = document.querySelector('#flubberHealth');
-
-//character buttons
-const buttonContainer = document.querySelector('#buttonContainer')
-const boxBoiButton = document.querySelector('#boxBoiButton')
-const devilDudeButton = document.querySelector('#devilDudeButton')
-const flubberButton = document.querySelector('#flubberButton')
-
+updateHealth = (player) => {
+    if(player === p1Character){
+        p1Health.innerText = "Health: " + player.health; 
+    }
+    else if(player === p2Character){
+        p2Health.innerText = "Health: " + player.health;
+    }
+}
 choiceChecker = () => {
     // console.log(`P1 Button's 'chosen' dataset: ${p1Button.dataset.chosen} \n P2 Button's 'chosen' dataset: ${p2Button.dataset.chosen} `)
     if(p1Button.dataset.chosen === 'true' && p2Button.dataset.chosen === 'true'){
         p1Button.setAttribute('class', 'hidden');
         p2Button.setAttribute('class', 'hidden');
+        h2.innerText = "Time to Fight!";
+        fightButtons.setAttribute('class', 'fightTime')
+        p1Attacks.setAttribute('class', 'moveList')
+        p2Attacks.setAttribute('class', 'moveList')
+        removeUnpicked();
+        updateHealth(p1Character);
+        updateHealth(p2Character);
     }
     else if(p1Button.dataset.chosen === 'false'){
         h2.innerText = "Now Select Player 1"
@@ -94,36 +118,85 @@ choiceChecker = () => {
         h2.innerText = "Now Select Player 2"
     }
 }
+removeUnpicked = () => {
+    if(boxBoi.dataset.chosen === 'false'){
+        console.log('should remove boxBoi')
+        boxBoi.setAttribute('class', 'hidden');
+    }
+    else if(devilDude.dataset.chosen === 'false'){
+        console.log('should remove devilDude')
+        devilDude.setAttribute('class', 'hidden');
+    }
+    else if(flubber.dataset.chosen === 'false'){
+        console.log('should remove flubber')
+        flubber.setAttribute('class', 'hidden');
+    }
+}
 
 objectMaker = (e) => {
     if(e.target !== buttonContainer){
         if(p1Button.dataset.toggle === 'on'){
-            let p1Character = new Character ('p1', e.target.dataset.name);
+            //target name for object
+            p1Character.fighter = e.target.dataset.name;
             p1Button.dataset.chosen = 'true';
             console.log(p1Character, p1.dataset.chosen);
             p1Button.disabled = true;
             p2Button.disabled = false;
+            e.target.disabled = true;
             alert('Player 1 has chosen ' + e.target.innerText);
+            e.target.innerText = "Locked";
             p1Button.dataset.toggle = 'off';
+            if(e.target.dataset.name === 'boxBoi'){
+                boxBoi.dataset.player = 'p1'
+            }
+            if(e.target.dataset.name === 'devilDude'){
+                devilDude.dataset.player = 'p1'
+            }
+            if(e.target.dataset.name === 'flubber'){
+                flubber.dataset.player = 'p1'
+                characterContainer.setAttribute('id', 'reverseCharacters');
+                buttonContainer.setAttribute('id', 'reverseContainer')
+            }
         }
         else if(p2Button.dataset.toggle === 'on'){
-            let p2Character = new Character ('p2', e.target.dataset.name);
+            //target name for object
+            p2Character.fighter = e.target.dataset.name; 
             p2Button.dataset.chosen = 'true';
+            e.target.dataset.chosen = 'true';
             console.log(p2Character, p2.dataset.chosen);
             p2Button.disabled = true;
             p1Button.disabled = false;
+            e.target.disabled = true;
             alert('Player 2 has chosen ' + e.target.innerText);
+            e.target.innerText = "Locked";
             p2Button.dataset.toggle = 'off';
+            if(e.target.dataset.name === 'boxBoi'){
+                boxBoi.dataset.player = 'p2'
+                characterContainer.setAttribute('id', 'reverseCharacters');
+                buttonContainer.setAttribute('id', 'reverseContainer')
+            }
+            if(e.target.dataset.name === 'devilDude'){
+                devilDude.dataset.player = 'p2'
+            }
+            if(e.target.dataset.name === 'flubber'){
+                flubber.dataset.player = 'p2'
+            }
+        }
+        if(e.target.dataset.name === 'boxBoi'){
+            boxBoi.dataset.chosen = 'true';
+        }
+        if(e.target.dataset.name === 'devilDude'){
+            devilDude.dataset.chosen = 'true';
+        }
+        if(e.target.dataset.name === 'flubber'){
+            flubber.dataset.chosen = 'true';
         }
         boxBoiButton.setAttribute('class', 'hidden');
-            devilDudeButton.setAttribute('class', 'hidden');
-            flubberButton.setAttribute('class', 'hidden');
-            choiceChecker();
+        devilDudeButton.setAttribute('class', 'hidden');
+        flubberButton.setAttribute('class', 'hidden');
+        choiceChecker();
     }
 }
-//need callback function for character button event listener; it should instantiate an object with the player that's choosing the object
-
-//need function that transitions game to fight screen once both players have chosen their characters
 
 //need 3 different attack buttons for each player
 //need functions for damage at each button
