aa1ea2829dcfbb3b4b41b8b5f60a206ef31bfc8b
ShnanignFlanign <seanpflan@live.com>
2022-07-16 12:41:53 -0700

    character select screen complete

--- app.js
+++ app.js
@@ -24,12 +24,14 @@ class Character {
 
 highlightSelection = (e) => {
     if(e.target !== selectPlayer){
+        console.log(e.target.id, e.target.dataset.toggle)
         if(e.target.dataset.toggle === 'off'){
             e.target.dataset.toggle = 'on';
+            console.log(e.target.dataset.toggle)
             boxBoiButton.setAttribute('class', 'visible'); 
             devilDudeButton.setAttribute('class', 'visible');
             flubberButton.setAttribute('class', 'visible');
-            h2.setAttribute('class', 'visible');
+            h2.innerText = "Now Choose Your Character.";
             if(e.target.id === 'p1'){
                 p2Button.disabled = true;
             }
@@ -39,6 +41,7 @@ highlightSelection = (e) => {
         }
         else if(e.target.dataset.toggle === 'on'){
             e.target.dataset.toggle = 'off';
+            // console.log(e.target.dataset.toggle)
             boxBoiButton.setAttribute('class', 'hidden')
             devilDudeButton.setAttribute('class', 'hidden');
             flubberButton.setAttribute('class', 'hidden');
@@ -79,12 +82,16 @@ const devilDudeButton = document.querySelector('#devilDudeButton')
 const flubberButton = document.querySelector('#flubberButton')
 
 choiceChecker = () => {
-    console.log(`P1 Button's 'chosen' dataset: ${p1Button.dataset.chosen} \n P2 Button's 'chosen' dataset: ${p2Button.dataset.chosen} `)
-    if(p1Button.dataset.chosen === true && p2Button.dataset.chosen === true){
-        boxBoiButton.setAttribute('class', 'hidden');
-        devilDudeButton.setAttribute('class', 'hidden');
-        flubberButton.setAttribute('class', 'hidden');
-        h2.setAttribute('class', 'hidden');
+    // console.log(`P1 Button's 'chosen' dataset: ${p1Button.dataset.chosen} \n P2 Button's 'chosen' dataset: ${p2Button.dataset.chosen} `)
+    if(p1Button.dataset.chosen === 'true' && p2Button.dataset.chosen === 'true'){
+        p1Button.setAttribute('class', 'hidden');
+        p2Button.setAttribute('class', 'hidden');
+    }
+    else if(p1Button.dataset.chosen === 'false'){
+        h2.innerText = "Now Select Player 1"
+    }
+    else if(p2Button.dataset.chosen === 'false'){
+        h2.innerText = "Now Select Player 2"
     }
 }
 
@@ -93,27 +100,25 @@ objectMaker = (e) => {
         if(p1Button.dataset.toggle === 'on'){
             let p1Character = new Character ('p1', e.target.dataset.name);
             p1Button.dataset.chosen = 'true';
-            boxBoiButton.setAttribute('class', 'hidden')
-            devilDudeButton.setAttribute('class', 'hidden');
-            flubberButton.setAttribute('class', 'hidden');
             console.log(p1Character, p1.dataset.chosen);
             p1Button.disabled = true;
             p2Button.disabled = false;
             alert('Player 1 has chosen ' + e.target.innerText);
-            choiceChecker()
+            p1Button.dataset.toggle = 'off';
         }
         else if(p2Button.dataset.toggle === 'on'){
             let p2Character = new Character ('p2', e.target.dataset.name);
             p2Button.dataset.chosen = 'true';
-            boxBoiButton.setAttribute('class', 'hidden')
-            devilDudeButton.setAttribute('class', 'hidden');
-            flubberButton.setAttribute('class', 'hidden');
             console.log(p2Character, p2.dataset.chosen);
             p2Button.disabled = true;
+            p1Button.disabled = false;
             alert('Player 2 has chosen ' + e.target.innerText);
-            choiceChecker()
+            p2Button.dataset.toggle = 'off';
         }
-        
+        boxBoiButton.setAttribute('class', 'hidden');
+            devilDudeButton.setAttribute('class', 'hidden');
+            flubberButton.setAttribute('class', 'hidden');
+            choiceChecker();
     }
 }
 //need callback function for character button event listener; it should instantiate an object with the player that's choosing the object
